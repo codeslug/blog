cd3a8b645223a94f5831fc80d0cd11a2c6feb7a3
codeslug <crazyshannon@gmail.comm>
2022-03-22 19:59:36 -0700

    cleaned stable version

--- blog/urls.py
+++ blog/urls.py
@@ -5,18 +5,12 @@ from .views import (
 	BlogCreateView, 
 	BlogUpdateView, 
 	BlogDeleteView,
-	PostListView,
-	# SearchView,
 )
 
 urlpatterns = [
-
-	path('', PostListView.as_view(), name='all'),
-	# path("search/", SearchView.as_view(), name="search"),
-	# path("contact/", ContactUs.as_view(), name="contact_form"),
 	path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),
 	path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
 	path("post/new/", BlogCreateView.as_view(), name="post_new"),
 	path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
-	# path("", BlogListView.as_view(), name="home"),
+	path("", BlogListView.as_view(), name="home"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,9 +7,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy, reverse
 from django import forms
 from django.views import View
-from django.contrib.humanize.templatetags.humanize import naturaltime
 
-from .utils import dump_queries
 from .models import Post
 from .forms import ContactForm, FeedbackForm, SurveyForm
 
@@ -37,46 +35,6 @@ class BlogDeleteView(DeleteView):
 	model = Post
 	template_name = "post_delete.html"
 	success_url = reverse_lazy("home")
-
-# class ContactUs(FormView):
-#     form = ContactForm
-#     template = 'contact_form.html'
-
-#     def get_success_url(self):
-#         return reverse('some-form-success-view')
-
-#     def form_valid(self, form):
-#         # Do something with the form data like send an email.
-#         return super().form_valid(form)
-
-# class SearchView(request, *args, **kwargs):
-	
-# 	template_name = "search.html"
-
-	# return HTTPResponse("<h1>Your search results</h1>")
-
-	# if request.method == "GET": # if the form is submitted
-	# 	search_query = request.GET.get("search_box", None)	
-		# do whatever you need with what the user looked for
-	# more code
-
-class PostListView(View):
-    template_name = "list.html"
-    def get(self, request) :
-        strval =  request.GET.get("search", False)
-        if strval :
-            # Simple title-only search
-            # objects = Post.objects.filter(title__contains=strval).select_related().order_by('-updated_at')[:10]
-            # Multi-field search
-            # __icontains for case-insensitive search
-            query = Q(title__icontains=strval) 
-            query.add(Q(text__icontains=strval), Q.OR)
-            post_list = Post.objects.filter(query).select_related()
-        else :
-            post_list = Post.objects.all()
-        # Augment the post_list
-        # for obj in post_list:
-        #     obj.natural_updated = naturaltime(obj.updated_at)
 
 
 
