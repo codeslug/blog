0cbf524022c3a9219a0d8ee60223312d25254953
codeslug <crazyshannon@gmail.comm>
2022-03-25 19:59:40 -0700

    Stable with BlogListView fixed

--- blog/urls.py
+++ blog/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
 	path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
 	path("post/new/", BlogCreateView.as_view(), name="post_new"),
 	path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
-	path("name/", BlogListView.getname(), name="search"),
+	path("", BlogListView.as_view(), name="home"),
+	# path("name/", BlogListView.getname(), name="search"),
 
 ]
